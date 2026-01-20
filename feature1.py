cbe877e790c526ab9c76822dcd85b25131522790
24dp1000038 <24dp1000038@ds.study.iitm.ac.in>
2026-01-20 21:18:30 +0530

    adding factor in featuer1

--- feature1.py
+++ feature1.py
@@ -3,3 +3,10 @@ def is_even(n):
         return True
     else:
         return False
+
+def factors(n):
+    result = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            result.append(i)
+    return result
